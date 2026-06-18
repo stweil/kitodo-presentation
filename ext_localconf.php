4ee4c61506d2f9c1df1c26ade08f353cc211ba0f
Stefan Weil <sw@weilnetz.de>
2026-06-18 08:48:00 +0200

    BUGFIX: Replace deprecated addPageTSConfig() for TYPO3 14 compatibility
    ExtensionManagementUtility::addPageTSConfig() was removed in TYPO3 14. Use direct assignment instead.
    
    Assisted-by: minimax-m2.7 (MiniMax)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -45,9 +45,11 @@ foreach ($iconArray as $key => $value) {
     );
 }
 // Register plugins as content elements.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:dlf/Configuration/TsConfig/ContentElements.tsconfig">'
-);
+// Use $GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'] for TYPO3 13+
+// as ExtensionManagementUtility::addPageTSConfig() was removed in TYPO3 14.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'] .= '
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:dlf/Configuration/TsConfig/ContentElements.tsconfig">
+';
 $_EXTKEY = 'dlf';
 // Register tools for toolbox plugin.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['dlf/Classes/Plugin/Toolbox.php']['tools'] = [];
