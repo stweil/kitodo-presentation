f6cd6fd27537c2e0c6c6079d610e08fee6622359
Stefan Weil <sw@weilnetz.de>
2026-06-18 09:04:17 +0200

    BUGFIX: Initialize defaultPageTSconfig before appending
    Fix undefined array key warning when adding Page TSConfig.
    
    Assisted-by: minimax-m2.7 (MiniMax)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,6 +17,9 @@ if (!defined('TYPO3')) {
 // Register plugins as content elements.
 // Use $GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'] for TYPO3 13+
 // as ExtensionManagementUtility::addPageTSConfig() was removed in TYPO3 14.
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'] = '';
+}
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['defaultPageTSconfig'] .= '
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:dlf/Configuration/TsConfig/ContentElements.tsconfig">
 ';
